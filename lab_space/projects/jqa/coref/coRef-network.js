049497f01b1fdf9e6636b1eba994c3248c05080e
Bill Quinn <wreedquinn@gmail.com>
2022-04-13 13:40:41 -0500

    Update coRef-network.js
    Added "/" to d3.json input. Was receiving 404 error on live site.

--- lab_space/projects/jqa/coref/coRef-network.js
+++ lab_space/projects/jqa/coref/coRef-network.js
@@ -28,7 +28,7 @@ function dragEnded(d, event) {
     d.fy = null;
 }
 
-d3.json('data/JQA_coRef-network.json').then(data => {
+d3.json('/data/JQA_coRef-network.json').then(data => {
      // Build constants.
     let margin = {top: 30, right: 30, bottom: 30, left: 30},
         width = 960,
